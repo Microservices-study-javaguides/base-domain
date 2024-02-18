cc03c268206388d77817ccca8359f8767b2e9dd8
vit19988 <Mr.peerawit@live.com>
2024-02-19 00:37:34 +0700

    first commit

---
+++ src/main/java/org/whisky/basedomain/dto/OrderEvent.java
@@ -0,0 +1,16 @@
+package org.whisky.basedomain.dto;
+
+import jdk.jfr.DataAmount;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderEvent
+{
+    private String message;
+    private String status;
+    private Order order;
+}
